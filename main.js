96172fe36277f18551e16d0ec9c7855b4e57adf7
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-08 12:33:57 -0700

    define main gameplay variables

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * 3)]; 
 }
 
+let computerSelection = getComputerChoice();
+console.log("compSel:" + computerSelection);
+
+const playerSelection = prompt("Rock, paper, or scissors?").toUpperCase()
+
