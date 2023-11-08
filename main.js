a4ae2f44c3de857be16e43e1f24b677c77571897
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-08 13:22:15 -0700

    compare user selection to program and inform the event of a tie

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-const choice = ["ROCK", "PAPER", "SCISSORS"];
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+
+// console.log(choices)
+// console.log(choices[0])
+// console.log(choices[1])
+// console.log(choices[2])
+
 
 function getComputerChoice() {
-    return choice[Math.floor(Math.random() * 3)]; 
+    return choices[Math.floor(Math.random() * 3)]; 
 }
 
-let computerSelection = getComputerChoice();
-console.log("compSel:" + computerSelection);
-
+const computerSelection = getComputerChoice();
+console.log(getComputerChoice());
+console.log("compSel: " + computerSelection);
 const playerSelection = prompt("Rock, paper, or scissors?").toUpperCase()
+console.log("userSel: " + playerSelection)
 
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log("Tie!");
+    } 
+}
+
+playRound()
