86de975432ce6b4c9a2a250471e5b6f75a7c0810
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-08 13:43:40 -0700

    add remaining win/ loss conditions

--- main.js
+++ main.js
@@ -17,13 +17,19 @@ console.log("userSel: " + playerSelection)
 
 function playRound () {
     if (playerSelection === computerSelection) {
-        console.log(playerSelection);
-        console.log(computerSelection);
         console.log("Tie!");
     } else if (playerSelection === choices[0] && computerSelection === choices[1]) {
-        console.log("Paper beats rock, try again!");
+        console.log("Paper beats rock, try again.");
     } else if (playerSelection === choices[0] && computerSelection === choices[2]) {
         console.log("Rock beats scissors, nice!");
+    } else if (playerSelection === choices[1] && computerSelection === choices[0]) {
+        console.log("Paper beats rock, nice!");
+    } else if (playerSelection === choices[1] && computerSelection === choices[2]) {
+        console.log("Scissors beat paper, try again.");
+    } else if (playerSelection === choices[2] && computerSelection === choices[0]) {
+        console.log("Rock beats scissors, sorry.");
+    } else if (playerSelection === choices[2] && computerSelection === choices[1]) {
+        console.log("Scissors beats paper, nice!");
     }
 }
 
