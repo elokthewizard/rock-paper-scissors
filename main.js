a5109dca749a90c660b76f06ef56f6a267a5e4c8
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-17 12:56:36 -0700

    make buttons respond to the proper id/ store the proper value

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let userScore = 0
+let computerScore = 0
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 
 let playerSelection;
@@ -6,18 +8,21 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)]; 
 }
 
+
 const div = document.createElement('div')
 div.classList.add('gameSpace');
 document.body.appendChild(div);
 
 const rockButton = document.createElement('button');
-rockButton.id = rockButton
+rockButton.id = 'rockButton'
 rockButton.textContent = "Rock";
 
 const paperButton = document.createElement('button');
+paperButton.id = 'paperButton'
 paperButton.textContent = "Paper";
 
 const scissorButton = document.createElement('button');
+scissorButton.id = 'scissorButton'
 scissorButton.textContent = "Scissors";
 
 div.appendChild(rockButton);
@@ -25,47 +30,46 @@ div.appendChild(paperButton);
 div.appendChild(scissorButton);
 
 div.addEventListener('click', (event) => {
-    if (event.target.id == rockButton) {
+    if (event.target.id == 'rockButton') {
         playerSelection = "Rock"
         console.log(playerSelection)
-    } else if (event.target.id == paperButton){
+        playRound()
+    } else if (event.target.id == 'paperButton'){
         playerSelection = 'Paper'
         console.log(playerSelection)
-    } else if (event.target.id == scissorButton){
+    } else if (event.target.id == 'scissorButton'){
         playerSelection = "Scissors"
+        console.log(playerSelection)
     }
 })
 
+function playRound () {
+    
+    const computerSelection = getComputerChoice();
 
-
-let userScore = 0
-let computerScore = 0
-
- function playRound () {
-     const computerSelection = getComputerChoice();
-
-     if (playerSelection === computerSelection) {
+    if (playerSelection === computerSelection) {
          console.log("Tie!");
-     } else if (playerSelection === choices[0] && computerSelection === choices[1]) {
+    } else if (playerSelection === choices[0] && computerSelection === choices[1]) {
          console.log("Paper beats rock, try again.");
          computerScore = computerScore++;
-     } else if (playerSelection === choices[0] && computerSelection === choices[2]) {
+    } else if (playerSelection === choices[0] && computerSelection === choices[2]) {
          console.log("Rock beats scissors, nice!");
          userScore = userScore++;
-     } else if (playerSelection === choices[1] && computerSelection === choices[0]) {
+    } else if (playerSelection === choices[1] && computerSelection === choices[0]) {
          console.log("Paper beats rock, nice!");
          userScore = userScore++;
-     } else if (playerSelection === choices[1] && computerSelection === choices[2]) {
+    } else if (playerSelection === choices[1] && computerSelection === choices[2]) {
          console.log("Scissors beat paper, try again.");
          computerScore = computerScore++;
-     } else if (playerSelection === choices[2] && computerSelection === choices[0]) {
+    } else if (playerSelection === choices[2] && computerSelection === choices[0]) {
          console.log("Rock beats scissors, sorry.");
          computerScore = computerScore++;
-     } else if (playerSelection === choices[2] && computerSelection === choices[1]) {
+    } else if (playerSelection === choices[2] && computerSelection === choices[1]) {
          console.log("Scissors beats paper, nice!");
          userScore = userScore++;
-     }
- }
+    }
+}
+
 
 // function playGame(){
 //     playRound();
