bcc70a94b16d74e88e374cd39b2283ff6547482d
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-17 15:00:34 -0700

    make new results replcae old ones
    i dont like the mess

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let userScore = 0
+let playerScore = 0
 let computerScore = 0
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 
 let playerSelection;
 
-const playSpace = document.createElement('div')
-playSpace.classList.add('playSpace');
-document.body.appendChild(playSpace);
+const gameSpace = document.createElement('div')
+gameSpace.classList.add('gameSpace');
+document.body.appendChild(gameSpace);
 
 const rockButton = document.createElement('button');
 rockButton.id = 'rockButton'
@@ -20,15 +20,15 @@ const scissorButton = document.createElement('button');
 scissorButton.id = 'scissorButton'
 scissorButton.textContent = "Scissors";
 
-playSpace.appendChild(rockButton);
-playSpace.appendChild(paperButton);
-playSpace.appendChild(scissorButton);
+gameSpace.appendChild(rockButton);
+gameSpace.appendChild(paperButton);
+gameSpace.appendChild(scissorButton);
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)]; 
 }
 
-playSpace.addEventListener('click', (e) => {
+gameSpace.addEventListener('click', (e) => {
     switch (e.target.id) {
         case 'rockButton':
             playerSelection = rockButton.textContent.toUpperCase();
@@ -43,48 +43,50 @@ playSpace.addEventListener('click', (e) => {
     playRound()
 })
 
+let resultMessage = document.createElement('div');
+let resultSpace = document.createElement('div');
+
 function playRound () {
     const computerSelection = getComputerChoice();
 
+    resultMessage.textContent = `You chose ${playerSelection}, and I chose ${computerSelection}`;
+
     console.log("playing...")
-    const resultMessage = document.createElement('div');
-    const resultSpace = document.createElement('div')
-    resultMessage.textContent = `You chose ${playerSelection}, and I chose ${computerSelection}`
 
     if (playerSelection === computerSelection) {
-        playSpace.appendChild(resultMessage);
+        gameSpace.appendChild(resultMessage);
         resultSpace.textContent = "It's a tie!";
-        playSpace.appendChild(resultSpace);
+        gameSpace.appendChild(resultSpace);
     } else if (playerSelection === choices[0] && computerSelection === choices[1]) {
-        playSpace.appendChild(resultMessage);
+        gameSpace.appendChild(resultMessage);
         resultSpace.textContent = "Paper beats rock, try again.";
-        playSpace.appendChild(resultSpace);
+        gameSpace.appendChild(resultSpace);
         computerScore = computerScore++;
     } else if (playerSelection === choices[0] && computerSelection === choices[2]) {
-        playSpace.appendChild(resultMessage);
+        gameSpace.appendChild(resultMessage);
         resultSpace.textContent = "Rock beats scissors, nice!";
-        playSpace.appendChild(resultSpace);
-        userScore = userScore++;
+        gameSpace.appendChild(resultSpace);
+        playerScore = playerScore++;
     } else if (playerSelection === choices[1] && computerSelection === choices[0]) {
-        playSpace.appendChild(resultMessage);
+        gameSpace.appendChild(resultMessage);
         resultSpace.textContent = "Paper beats rock, nice!";
-        playSpace.appendChild(resultSpace);
-        userScore = userScore++;
+        gameSpace.appendChild(resultSpace);
+        playerScore = playerScore++;
     } else if (playerSelection === choices[1] && computerSelection === choices[2]) {
-        playSpace.appendChild(resultMessage);
+        gameSpace.appendChild(resultMessage);
         resultSpace.textContent = "Scissors beat paper, try again.";
-        playSpace.appendChild(resultSpace);
+        gameSpace.appendChild(resultSpace);
         computerScore = computerScore++;
     } else if (playerSelection === choices[2] && computerSelection === choices[0]) {
-        playSpace.appendChild(resultMessage);
+        gameSpace.appendChild(resultMessage);
         resultSpace.textContent = "Rock beats scissors, sorry.";
-        playSpace.appendChild(resultSpace);
+        gameSpace.appendChild(resultSpace);
         computerScore = computerScore++;
     } else if (playerSelection === choices[2] && computerSelection === choices[1]) {
-        playSpace.appendChild(resultMessage);
+        gameSpace.appendChild(resultMessage);
         resultSpace.textContent = "Scissors beats paper, nice!";
-        playSpace.appendChild(resultSpace);
-        userScore = userScore++;
+        gameSpace.appendChild(resultSpace);
+        playerScore = playerScore++;
     }
     
 }
@@ -93,7 +95,7 @@ function playRound () {
 
 // function playGame(){
 //     playRound();
-//     if (userScore > computerScore) {
+//     if (playerScore > computerScore) {
 //         console.log("You win!")
 //     } else {
 //         console.log("Too bad, you lost!")
